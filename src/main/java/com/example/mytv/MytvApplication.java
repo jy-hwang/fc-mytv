d49d28c99dfe7b8d01ffded8e191888ac9af6f02
jyhwang <stratojy@gmail.com>
2026-08-05 23:00:48 +0900

    feat. 프로젝트 시작
    1. java 21
    2. spring boot 3.5.16

---
+++ src/main/java/com/example/mytv/MytvApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytvApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MytvApplication.class, args);
+  }
+
+}
